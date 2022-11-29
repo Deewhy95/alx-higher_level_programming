76d8a02b0c946abb5e40d8562d7bb08849828aa3
Deewhy95 <aosifuye@yahoo.com>
2022-11-29 21:38:33 +0000

    Numb

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
-#!/ur/bin/python3
+#!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 = str1 + " " + str2
-print("Welcome to {}!".format(str1))
+num = str1 + " " + str2
+print("Welcome to {}!".format(num))
